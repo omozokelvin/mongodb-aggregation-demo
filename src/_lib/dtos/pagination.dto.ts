86867f1dde9d9005373ec1643c9318490b2d293d
Kelvin Omozokpia <kelvinjune2@gmail.com>
2025-07-26 11:59:21 +0100

    feat: enhance pagination with request DTO and integrate into user query

--- src/_lib/dtos/pagination.dto.ts
+++ src/_lib/dtos/pagination.dto.ts
@@ -1,4 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsInt, Min, Max } from 'class-validator';
+
 export class PaginationMeta {
   total: number;
   limit?: number;
+}
+
+export class PaginationRequestDto {
+  @IsInt()
+  @Min(10)
+  @Max(2000)
+  @ApiProperty({
+    example: 100,
+  })
+  @Transform(({ value }) => +value)
+  limit: number;
 }
--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,25 +1,19 @@
-import { PaginationMeta } from '@/_lib/dtos/pagination.dto';
+import {
+  PaginationMeta,
+  PaginationRequestDto,
+} from '@/_lib/dtos/pagination.dto';
 import { User, UserStatus } from '@/user/entities/user.entity';
-import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsDate, IsEnum, IsInt, IsOptional, Max, Min } from 'class-validator';
+import { IsDate, IsEnum, IsOptional } from 'class-validator';
 
-export class QueryUserDto {
-  @IsInt()
-  @Min(10)
-  @Max(1000)
-  @ApiProperty({
-    example: 100,
-  })
-  @Transform(({ value }) => +value)
-  limit: number;
-
+export class QueryUserDto extends PaginationRequestDto {
   @IsOptional()
   @IsEnum(UserStatus)
   status?: UserStatus;
 
   @IsOptional()
   @IsDate()
+  @Transform(({ value }) => new Date(value))
   signupDate?: Date;
 }
 
